4acaa5e72cd4d86c419fece34e3759ba290d050d
James Roland Cabresos <jamesroland@dwimmyfactory.com>
2015-02-12 12:07:59 +0800

    added amd and standalone builds

---
+++ dist/signals.d.ts
@@ -0,0 +1,23 @@
+declare module "kola-signals" {
+    export class SignalDispatcher<T> {
+        listeners: {
+            [id: number]: SignalListener<T>;
+        };
+        private numListeners;
+        constructor();
+        addListener(listener: SignalListener<T>): void;
+        removeListener(listener: SignalListener<T>): void;
+        removeAllListeners(): void;
+        getListenersLength(): number;
+        dispatch(payload: T): void;
+    }
+    export class SignalListener<T> {
+        callback: (payload: T) => void;
+        target: any;
+        callOnce: boolean;
+        id: number;
+        constructor(callback: (payload: T) => void, target?: any, callOnce?: boolean);
+        receiveSignal(payload: T): void;
+    }
+}
+
